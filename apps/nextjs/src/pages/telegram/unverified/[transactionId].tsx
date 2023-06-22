e773f52763470c2cbd1f04798c3b0f74fa481732
Athul P <athul929@gmail.com>
2023-06-22 13:17:02 +0530

    Fix

--- apps/nextjs/src/pages/telegram/unverified/[transactionId].tsx
+++ apps/nextjs/src/pages/telegram/unverified/[transactionId].tsx
@@ -95,14 +95,16 @@ const Transaction = () => {
     }
   }, [addTransaction, Telegram, editForm]);
 
-  if (
+  const isLoading =
     unverifiedTransactionQuery.isLoading ||
     accountsQuery.isLoading ||
     payeesQuery.isLoading ||
-    categoriesQuery.isLoading
-  ) {
-    return <div>Loading...</div>;
-  }
+    categoriesQuery.isLoading;
+  const isError =
+    unverifiedTransactionQuery.isError ||
+    accountsQuery.isError ||
+    payeesQuery.isError ||
+    categoriesQuery.isError;
 
   return (
     <>
@@ -112,103 +114,73 @@ const Transaction = () => {
           setTelegram((window as any).Telegram.WebApp);
         }}
       />
-      <div className="h-screen">
-        <style jsx global>
-          {telegramStyles}
-        </style>
-        <header className="text-center text-2xl font-bold">
-          Review Transaction
-        </header>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error</div>}
+      {!isLoading && !isError && (
+        <div className="h-screen">
+          <style jsx global>
+            {telegramStyles}
+          </style>
+          <header className="text-center text-2xl font-bold">
+            Review Transaction
+          </header>
 
-        <form id="editForm" className="p-5">
-          <Controller
-            control={editForm.control}
-            name="sourceAccount"
-            rules={{ required: true }}
-            render={({ field }) => (
-              <Select
-                {...field}
-                options={accountsQuery.data.accounts}
-                form="editForm"
-                getOptionLabel={(option) => option.name}
-                getOptionValue={(option) => option.id}
-                menuPortalTarget={document.body}
-                styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-                isSearchable={false}
-                theme={(theme) => ({
-                  ...theme,
-                  colors: {
-                    ...theme.colors,
-                    primary25: Telegram?.themeParams?.hint_color,
-                    primary: Telegram?.themeParams?.button_color,
-                    neutral0: Telegram?.themeParams?.secondary_bg_color,
-                    neutral80: Telegram?.themeParams?.text_color,
-                  },
-                })}
-                classNames={{
-                  control: () =>
-                    " w-full text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                  input: () => "border-0 text-white py-4 bg-black",
-                }}
-              />
-            )}
-          ></Controller>
-          <ControlledSelect
-            control={editForm.control}
-            form="editForm"
-            name="type"
-            options={TransactionTypeOptions}
-            isSimple
-          ></ControlledSelect>
-          <Controller
-            control={editForm.control}
-            name="category"
-            rules={{ required: true }}
-            render={({ field }) => (
-              <Select
-                {...field}
-                options={categoriesQuery.data.categories}
-                className="my-2"
-                form="editForm"
-                isLoading={categoriesQuery.isLoading}
-                isDisabled={editForm.watch("type") == null}
-                getOptionLabel={(option) => option.name}
-                getOptionValue={(option) => option.id}
-                menuPortalTarget={document.body}
-                styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-                isSearchable={false}
-                theme={(theme) => ({
-                  ...theme,
-                  colors: {
-                    ...theme.colors,
-                    primary25: Telegram?.themeParams?.hint_color,
-                    primary: Telegram?.themeParams?.button_color,
-                    neutral0: Telegram?.themeParams?.secondary_bg_color,
-                    neutral80: Telegram?.themeParams?.text_color,
-                  },
-                })}
-                classNames={{
-                  control: () =>
-                    " w-full text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                  input: () => "border-0 text-white py-4",
-                }}
-              />
-            )}
-          ></Controller>
-          {editForm.watch("type") !== TRANSACTION_TYPE.TRANSFER ? (
+          <form id="editForm" className="p-5">
             <Controller
               control={editForm.control}
-              name="payee"
-              rules={{ required: false }}
+              name="sourceAccount"
+              rules={{ required: true }}
               render={({ field }) => (
                 <Select
                   {...field}
-                  options={payeesQuery.data.payees}
+                  options={accountsQuery.data.accounts}
+                  form="editForm"
                   getOptionLabel={(option) => option.name}
                   getOptionValue={(option) => option.id}
                   menuPortalTarget={document.body}
                   styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+                  isSearchable={false}
+                  theme={(theme) => ({
+                    ...theme,
+                    colors: {
+                      ...theme.colors,
+                      primary25: Telegram?.themeParams?.hint_color,
+                      primary: Telegram?.themeParams?.button_color,
+                      neutral0: Telegram?.themeParams?.secondary_bg_color,
+                      neutral80: Telegram?.themeParams?.text_color,
+                    },
+                  })}
+                  classNames={{
+                    control: () =>
+                      " w-full text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                    input: () => "border-0 text-white py-4 bg-black",
+                  }}
+                />
+              )}
+            ></Controller>
+            <ControlledSelect
+              control={editForm.control}
+              form="editForm"
+              name="type"
+              options={TransactionTypeOptions}
+              isSimple
+            ></ControlledSelect>
+            <Controller
+              control={editForm.control}
+              name="category"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={categoriesQuery.data.categories}
+                  className="my-2"
                   form="editForm"
+                  isLoading={categoriesQuery.isLoading}
+                  isDisabled={editForm.watch("type") == null}
+                  getOptionLabel={(option) => option.name}
+                  getOptionValue={(option) => option.id}
+                  menuPortalTarget={document.body}
+                  styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
                   isSearchable={false}
                   theme={(theme) => ({
                     ...theme,
@@ -228,56 +200,92 @@ const Transaction = () => {
                 />
               )}
             ></Controller>
-          ) : (
-            <ControlledSelect
-              control={editForm.control}
-              form="editForm"
-              isDisabled={editForm.watch("type") == null}
-              name="transferredAccount"
-              options={accountsQuery?.data.accounts.filter(
-                (account) => account.id !== editForm.watch("sourceAccount")?.id,
-              )}
-            ></ControlledSelect>
-          )}
-
-          <Controller
-            control={editForm.control}
-            name="timeCreated"
-            rules={{ required: true }}
-            render={({ field: { onChange, onBlur, value, name, ref } }) => (
-              <Datetime
-                timeFormat={false}
-                initialViewDate={new Date()}
-                dateFormat="Do MMMM YY"
-                className="my-2"
-                inputProps={{
-                  form: "editForm",
-                  placeholder: "Select date",
-                  onBlur: onBlur,
-                  name: name,
-                  className:
-                    "block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                }}
-                onChange={onChange}
-                value={value}
-                ref={ref}
-              />
+            {editForm.watch("type") !== TRANSACTION_TYPE.TRANSFER ? (
+              <Controller
+                control={editForm.control}
+                name="payee"
+                rules={{ required: false }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={payeesQuery.data.payees}
+                    getOptionLabel={(option) => option.name}
+                    getOptionValue={(option) => option.id}
+                    menuPortalTarget={document.body}
+                    styles={{
+                      menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                    }}
+                    form="editForm"
+                    isSearchable={false}
+                    theme={(theme) => ({
+                      ...theme,
+                      colors: {
+                        ...theme.colors,
+                        primary25: Telegram?.themeParams?.hint_color,
+                        primary: Telegram?.themeParams?.button_color,
+                        neutral0: Telegram?.themeParams?.secondary_bg_color,
+                        neutral80: Telegram?.themeParams?.text_color,
+                      },
+                    })}
+                    classNames={{
+                      control: () =>
+                        " w-full text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                      input: () => "border-0 text-white py-4",
+                    }}
+                  />
+                )}
+              ></Controller>
+            ) : (
+              <ControlledSelect
+                control={editForm.control}
+                form="editForm"
+                isDisabled={editForm.watch("type") == null}
+                name="transferredAccount"
+                options={accountsQuery?.data.accounts.filter(
+                  (account) =>
+                    account.id !== editForm.watch("sourceAccount")?.id,
+                )}
+              ></ControlledSelect>
             )}
-          />
-          <div className="flex">
-            <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
-              ₹
-            </span>
-            <input
-              form="editForm"
-              {...editForm.register("amount", { required: true })}
-              placeholder="Amount"
-              type="number"
-              className="block min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              required
-            ></input>
-          </div>
-          {/* <div className="mt-5 flex justify-start">
+
+            <Controller
+              control={editForm.control}
+              name="timeCreated"
+              rules={{ required: true }}
+              render={({ field: { onChange, onBlur, value, name, ref } }) => (
+                <Datetime
+                  timeFormat={false}
+                  initialViewDate={new Date()}
+                  dateFormat="Do MMMM YY"
+                  className="my-2"
+                  inputProps={{
+                    form: "editForm",
+                    placeholder: "Select date",
+                    onBlur: onBlur,
+                    name: name,
+                    className:
+                      "block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                  }}
+                  onChange={onChange}
+                  value={value}
+                  ref={ref}
+                />
+              )}
+            />
+            <div className="flex">
+              <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+                ₹
+              </span>
+              <input
+                form="editForm"
+                {...editForm.register("amount", { required: true })}
+                placeholder="Amount"
+                type="number"
+                className="block min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                required
+              ></input>
+            </div>
+            {/* <div className="mt-5 flex justify-start">
           <button
             type="submit"
             className="border w-32 p-2 br rounded-lg focus:ring ring-emerald-300 dark:ring-emerald-700 transition-colors duration-150 bg-emerald-600 dark:bg-emerald-500 text-white"
@@ -288,8 +296,9 @@ const Transaction = () => {
             Cancel
           </button>
         </div> */}
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
