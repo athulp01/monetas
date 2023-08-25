2095cc4d1716f0646669229afbc37cf384b84da4
Athul P <athul929@gmail.com>
2023-08-25 17:22:58 +0530

    Update clerk version

--- Dockerfile
+++ Dockerfile
@@ -63,5 +63,7 @@ USER nextjs
 EXPOSE 3000
 
 ENV PORT 3000
+ENV HOSTNAME "0.0.0.0"
+
 WORKDIR /app/apps/nextjs
 CMD ["node", "server.js"]
--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,8 +1,8 @@
-import type {
-  SignedInAuthObject,
-  SignedOutAuthObject,
-} from "@clerk/nextjs/api";
-import { getAuth } from "@clerk/nextjs/server";
+import {
+  getAuth,
+  type SignedInAuthObject,
+  type SignedOutAuthObject,
+} from "@clerk/nextjs/server";
 import { type inferAsyncReturnType } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 
